a32ea826fe5325116af281d0a95fae65136f6ec5
Sahil Malik <sahil.malik@comprotechnologies.com>
2020-04-16 18:45:18 +0530

    Change port number

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 4000 || process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.sendFile('./index.html', {root: __dirname});
